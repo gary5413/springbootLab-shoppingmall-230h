d78788cc97fee2ce08f1a5ab2bedc8925c907afb
gary5413 <5413gary@gmail.com>
2023-01-06 17:07:27 +0800

    init commit

---
+++ src/main/java/com/garylee/springbootlabshoppingmall230h/SpringbootLabShoppingmall230hApplication.java
@@ -0,0 +1,13 @@
+package com.garylee.springbootlabshoppingmall230h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLabShoppingmall230hApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLabShoppingmall230hApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/garylee/springbootlabshoppingmall230h/SpringbootLabShoppingmall230hApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garylee.springbootlabshoppingmall230h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLabShoppingmall230hApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
